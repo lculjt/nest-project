91d3d222a2502be5dec8765863fd8c9fe72e499f
lijianting <v_lijianting@baidu.com>
2025-04-08 14:02:51 +0800

    feat: 模拟扫码登录

---
+++ nest/qrcode-login/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+@Module({
+  imports: [JwtModule.register({
+    secret: 'ljt',
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
